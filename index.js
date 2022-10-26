0011c9f5f91d470a3310b4601f0c2b3fd36bf605
AminulIslamorange <aminulislamorange@gmail.com>
2022-10-26 18:48:57 +0600

    Course Data Done

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-
 const Port = process.env.Port || 5000;
 
+
+// Main Path
 app.get('/' , (req , res)=>{
    res.send("Welcome To Edukite Server")
 })
+
+// All Courses api
+app.get('/allcourses' , (req , res)=>{
+
+    res.send("Welcome To Edukite Server")
+ })
 
 app.listen(Port , ()=>{
     console.log(`Serbver is Running on : ${Port}`);
