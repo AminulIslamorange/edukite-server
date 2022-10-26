67a555a90ec9820e80faf68bffa2a9ce33edb0ca
AminulIslamorange <aminulislamorange@gmail.com>
2022-10-26 18:56:40 +0600

    get single course api done

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const cors = require('cors')
 app.use(cors())
 const Port = process.env.Port || 5000;
 
-
+const courses = require('./Data/data.json')
 // Main Path
-app.get('/' , (req , res)=>{
+app.get('/api' , (req , res)=>{
    res.send("Welcome To Edukite Server")
 })
 
 // All Courses api
-app.get('/allcourses' , (req , res)=>{
+app.get('/api/allcourses' , (req , res)=>{
+    res.send(courses)
+ })
 
-    res.send("Welcome To Edukite Server")
+// get single course by id
+app.get('/api/course/:id' , (req , res)=>{
+    let {id} = req.params
+    let course = courses.find(perCourse => perCourse.id == id )
+    res.send(course)
  })
 
 app.listen(Port , ()=>{
